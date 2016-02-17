a4e2b5d2aa815505be563b1ab83a75c790b4d8ec
Andrey Kuleshov <andrey.kuleshov@devexpress.com>
2016-02-17 17:50:51 +0300

    Initial commit

---
+++ src/WebApplication2/wwwroot/sources/app.ts
@@ -0,0 +1,11 @@
+﻿function collectionItemType(itemType: string) {
+    return (target: Object, key: string) => {
+        console.log("some work is performed here");
+        return;
+    }
+}
+
+class MyRealClass  {
+    @collectionItemType("Rectagle")
+    hiddenDimensions: Array<string>;
+}
